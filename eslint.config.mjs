ae3e663ab72dabce821851a5bb24d3a0183244e7
Moons <me@moons.bio>
2024-09-11 18:43:52 +0200

    feat: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {rules: { "indent": ["error", 2] }},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2138;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+app.get('/', (_, res: Response) => {
+  res.send('Hello!');
 });
 
+app.get('/health', (_, res: Response) => {
+  res.status(200).json({ message: 'ok' });
+})
+
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
